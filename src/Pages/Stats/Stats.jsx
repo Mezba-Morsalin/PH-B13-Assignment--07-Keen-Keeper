697c6437a046ba561d367ff287487411c2e1020c
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-04-15 18:55:08 +0600

    Drop Down Sort added

--- src/Pages/Stats/Stats.jsx
+++ src/Pages/Stats/Stats.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { FriendsContext } from '../../FriendsProvider/FriendsProvider';
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  ResponsiveContainer,
+  Sector
+} from 'recharts';
 
 const Stats = () => {
+  const { calls, texts, videos } = useContext(FriendsContext);
+  const total = calls.length + texts.length + videos.length;
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const data = [
+    { name: 'Calls', value: calls.length, fill: '#8884d8' },
+    { name: 'Texts', value: texts.length, fill: '#82ca9d' },
+    { name: 'Videos', value: videos.length, fill: '#ffc658' },
+  ];
+
+  const renderActiveShape = (props) => {
+    const {
+      cx, cy, innerRadius, outerRadius,
+      startAngle, endAngle, fill
+    } = props;
+
     return (
-        <div>
-            <h2>Stats Page</h2>
-        </div>
+      <g>
+        <Sector cx={cx} cy={cy} innerRadius={innerRadius} outerRadius={outerRadius + 10} startAngle={startAngle} endAngle={endAngle} fill={fill}/>
+      </g>
     );
+  };
+  return (
+    <div className="">
+        <h2 className='w-11/12 lg:w-9/12 mx-auto font-bold text-5xl my-6'>Interaction</h2>
+     <div className='w-11/12 lg:w-9/12 mx-auto mt-20 h-[400px] bg-white shadow rounded-2xl'>
+         {
+        total === 0 ? (
+          <div className="flex justify-center items-center h-full">
+            <h2 className="text-2xl font-bold text-gray-500">
+              No Interaction Found
+            </h2>
+          </div>
+        ) : (
+          <>
+          <h2 className='text-2xl font-bold text-gray-500 p-4'>By The Interaction</h2>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie data={data} dataKey="value" cx="50%" cy="50%" innerRadius={60} outerRadius={120} activeIndex={activeIndex} activeShape={renderActiveShape} onMouseEnter={(_, index) => setActiveIndex(index)} isAnimationActive={true} animationDuration={800} animationEasing="ease-out" label/>
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+            <div className='flex gap-8 justify-center items-center '>
+              <div className='flex gap-2.5 items-center'>
+                <div className='w-3 h-3 bg-[#8884d8]'></div>
+                <p className='text-[#8884d8] font-bold'>Calls</p>
+              </div>
+              <div className='flex gap-2.5 items-center'>
+                <div className='w-3 h-3 bg-[#82ca9d]'></div>
+                <p className='text-[#82ca9d] font-bold'>Text</p>
+              </div>
+              <div className='flex gap-2.5 items-center'>
+                <div className='w-3 h-3 bg-[#ffc658]'></div>
+                <p className='text-[#ffc658] font-bold'>Video</p>
+              </div>
+            </div>
+          </>
+        )
+      }
+
+     </div>
+    </div>
+  );
 };
 
 export default Stats;
--- src/Pages/Timeline/Timeline.jsx
+++ src/Pages/Timeline/Timeline.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FriendsContext } from '../../FriendsProvider/FriendsProvider';
 import callImg from '../../assets/call.png'
 import textImg from '../../assets/text.png'
@@ -6,69 +6,91 @@ import videoImg from '../../assets/video.png'
 
 const Timeline = () => {
     const {calls, texts, videos} = useContext(FriendsContext)
+    const [filter, setFilter] = useState("all")
+    let showCalls = filter === "all" || filter === "calls"
+    let showTexts = filter === "all" || filter === "texts"
+    let showVideos = filter === "all" || filter === "videos"
     return (
         <div className='w-11/12 lg:w-9/12 mx-auto mt-16'>
-            <h2 className='font-bold text-5xl mb-12'>Timeline</h2>
+            <h2 className='font-bold text-5xl mb-6'>Timeline</h2>
+            <div className='flex justify-center items-center'>
+                <div className="dropdown dropdown-center mb-12">
+                <div tabIndex={0} role="button" className="btn m-1">{filter.toUpperCase()} ⬇️</div>
+                    <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a className='text-black hover:bg-[#244D3F] hover:text-white' onClick={()=>setFilter("all")}>All</a></li>
+                        <li><a className='text-black hover:bg-[#244D3F] hover:text-white'onClick={()=>setFilter("calls")}>Calls</a></li>
+                        <li><a className='text-black hover:bg-[#244D3F] hover:text-white'onClick={()=>setFilter("texts")}>Texts</a></li>
+                        <li><a className='text-black hover:bg-[#244D3F] hover:text-white'onClick={()=>setFilter("videos")}>Videos</a></li>
+                    </ul>
+                </div>
+            </div>
             <div>
                 <div className='bg-white rounded-2xl shadow p-5 space-y-3.5'>
             <div>
-                {calls.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
-                    No Call History Found
-                </div> : 
-                <div  className='space-y-3'>
-                    {
-                        calls.map(call => {
-                            return (
-                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
-                                    <img src={callImg} alt={callImg} />
-                                <div>
-                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Call </span>With <span>{call.name}</span></h3>
-                                <p className='text-[#64748B]'>{call.next_due_date}</p>
-                                </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                {
+                    showCalls && (
+                        calls.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                            No Call History Found
+                        </div> : 
+                        <div className='space-y-3'>
+                            {
+                                calls.map(call => (
+                                    <div key={call.id} className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                        <img src={callImg} alt={callImg} />
+                                        <div>
+                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Call </span>With <span>{call.name}</span></h3>
+                                            <p className='text-[#64748B]'>{call.next_due_date}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
                 }
             </div>
             <div>
-                {texts.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
-                    No Text History Found
-                </div> : <div  className='space-y-3'>
-                    {
-                        texts.map(text => {
-                            return (
-                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
-                                    <img src={textImg} alt={textImg} />
-                                <div>
-                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{text.name}</span></h3>
-                                <p className='text-[#64748B]'>{text.next_due_date}</p>
-                                </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>}
+                {
+                    showTexts && (
+                        texts.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                            No Text History Found
+                        </div> : 
+                        <div className='space-y-3'>
+                            {
+                                texts.map(text => (
+                                    <div key={text.id} className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                        <img src={textImg} alt={textImg} />
+                                        <div>
+                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{text.name}</span></h3>
+                                            <p className='text-[#64748B]'>{text.next_due_date}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                }
             </div>
-            <div >
-                {videos.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
-                    No Video Calls History Found
-                </div> : <div  className='space-y-3'>
-                    {
-                        videos.map(video => {
-                            return (
-                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
-                                    <img src={videoImg} alt={videoImg} />
-                                <div>
-                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{video.name}</span></h3>
-                                <p className='text-[#64748B]'>{video.next_due_date}</p>
-                                </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>}
+            <div>
+                {
+                    showVideos && (
+                        videos.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                            No Video Calls History Found
+                        </div> : 
+                        <div className='space-y-3'>
+                            {
+                                videos.map(video => (
+                                    <div key={video.id} className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                        <img src={videoImg} alt={videoImg} />
+                                        <div>
+                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Video </span>With <span>{video.name}</span></h3>
+                                            <p className='text-[#64748B]'>{video.next_due_date}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                }
             </div>
         </div>
             </div>
