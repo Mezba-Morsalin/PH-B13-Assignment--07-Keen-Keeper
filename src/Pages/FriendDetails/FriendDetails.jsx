971d0028b53d2fd982b342a6423a384518008b7e
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-04-15 17:08:27 +0600

    Timeline Page Completed

--- src/Pages/FriendDetails/FriendDetails.jsx
+++ src/Pages/FriendDetails/FriendDetails.jsx
@@ -8,13 +8,16 @@ import { LuArchiveRestore } from 'react-icons/lu';
 import { MdHistory, MdOutlineDeleteForever } from 'react-icons/md';
 import { useLoaderData, useParams } from 'react-router';
 import { FriendsContext } from '../../FriendsProvider/FriendsProvider';
+import callImg from '../../assets/call.png'
+import textImg from '../../assets/text.png'
+import videoImg from '../../assets/video.png'
 
 const FriendDetails = () => {
     const {id} = useParams();
     const friendData = useLoaderData();
     const findFriend = friendData.find(friend => friend.id === Number(id))
     
-    const {handleCall, handleMassage, handleVideo} = useContext(FriendsContext);
+    const {handleCall, handleMassage, handleVideo, calls, texts, videos} = useContext(FriendsContext);
     return (
         <div className='w-11/12 lg:w-9/12 mx-auto mt-20'>
             <div className='grid grid-cols-1 md:grid-cols-12 gap-10'>
@@ -80,11 +83,71 @@ const FriendDetails = () => {
                         </div>
                     </div>
                     <div className='bg-white shadow p-6 rounded-2xl space-y-2'>
-                        <div className='flex justify-between items-center'>
+                        <div className='flex justify-between items-center mb-6'>
                             <h3 className='text-[#244D3F] text-xl'>Recent Interactions</h3>
                             <button className='btn'><MdHistory></MdHistory>Full History</button>
                         </div>
-                        <div></div>
+                        <div className='bg-white rounded-2xl p-5 space-y-3.5'>
+                                    <div>
+                                        {calls.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                                            No Call History Found
+                                        </div> : 
+                                        <div  className='space-y-3'>
+                                            {
+                                                calls.map(call => {
+                                                    return (
+                                                        <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                                            <img src={callImg} alt={callImg} />
+                                                        <div>
+                                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Call </span>With <span>{call.name}</span></h3>
+                                                        <p className='text-[#64748B]'>{call.next_due_date}</p>
+                                                        </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        }
+                                    </div>
+                                    <div>
+                                        {texts.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                                            No Text History Found
+                                        </div> : <div  className='space-y-3'>
+                                            {
+                                                texts.map(text => {
+                                                    return (
+                                                        <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                                            <img src={textImg} alt={textImg} />
+                                                        <div>
+                                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{text.name}</span></h3>
+                                                        <p className='text-[#64748B]'>{text.next_due_date}</p>
+                                                        </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>}
+                                    </div>
+                                    <div >
+                                        {videos.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                                            No Video Calls History Found
+                                        </div> : <div  className='space-y-3'>
+                                            {
+                                                videos.map(video => {
+                                                    return (
+                                                        <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                                            <img src={videoImg} alt={videoImg} />
+                                                        <div>
+                                                            <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{video.name}</span></h3>
+                                                        <p className='text-[#64748B]'>{video.next_due_date}</p>
+                                                        </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>}
+                                    </div>
+                                </div>
                     </div>
                 </div>
             </div>
--- src/Pages/Timeline/Timeline.jsx
+++ src/Pages/Timeline/Timeline.jsx
@@ -1,13 +1,77 @@
 import React, { useContext } from 'react';
 import { FriendsContext } from '../../FriendsProvider/FriendsProvider';
+import callImg from '../../assets/call.png'
+import textImg from '../../assets/text.png'
+import videoImg from '../../assets/video.png'
 
 const Timeline = () => {
     const {calls, texts, videos} = useContext(FriendsContext)
     return (
-        <div>
-            <h2>{calls.length}</h2>
-            <h2>{texts.length}</h2>
-            <h2>{videos.length}</h2>
+        <div className='w-11/12 lg:w-9/12 mx-auto mt-16'>
+            <h2 className='font-bold text-5xl mb-12'>Timeline</h2>
+            <div>
+                <div className='bg-white rounded-2xl shadow p-5 space-y-3.5'>
+            <div>
+                {calls.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                    No Call History Found
+                </div> : 
+                <div  className='space-y-3'>
+                    {
+                        calls.map(call => {
+                            return (
+                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                    <img src={callImg} alt={callImg} />
+                                <div>
+                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Call </span>With <span>{call.name}</span></h3>
+                                <p className='text-[#64748B]'>{call.next_due_date}</p>
+                                </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                }
+            </div>
+            <div>
+                {texts.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                    No Text History Found
+                </div> : <div  className='space-y-3'>
+                    {
+                        texts.map(text => {
+                            return (
+                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                    <img src={textImg} alt={textImg} />
+                                <div>
+                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{text.name}</span></h3>
+                                <p className='text-[#64748B]'>{text.next_due_date}</p>
+                                </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>}
+            </div>
+            <div >
+                {videos.length === 0 ? <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300'>
+                    No Video Calls History Found
+                </div> : <div  className='space-y-3'>
+                    {
+                        videos.map(video => {
+                            return (
+                                <div className='bg-[#F8FAFC] rounded-2xl p-5 border border-gray-300 flex items-center gap-5'>
+                                    <img src={videoImg} alt={videoImg} />
+                                <div>
+                                    <h3 className='text-[#64748B]'><span className='text-black font-bold text-xl'>Text </span>With <span>{video.name}</span></h3>
+                                <p className='text-[#64748B]'>{video.next_due_date}</p>
+                                </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>}
+            </div>
+        </div>
+            </div>
         </div>
     );
 };
